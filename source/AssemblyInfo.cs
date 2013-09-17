b3684fd5503a061a378101dba643cdb12dee1906
Pavel Mamut <pavel@versionone.com>
2013-09-17 10:56:50 -0400

    added AssemblyInfo.cs

---
+++ source/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyTitle("VersionOne Localization Library")]
+[assembly: AssemblyDescription("Localization and internationalization library with support for multi-layer overrides")]
+[assembly: AssemblyCompany("VersionOne, Inc.")]
+[assembly: AssemblyProduct("VersionOne.Localization")]
+[assembly: AssemblyCopyright("Copyright © 2013, VersionOne, Inc. All rights reserved.")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
